cde8ecf955d467d14b9a1bfc0a1d44ed47167394
Igor Tkachenko <IATkachenko@gmail.com>
2022-01-21 10:48:12 +0300

    feat: add config_flow
    We should trigger update only after adding sensor to hass to avoid too early update calls while Home Assistant restart.
    
    User should not change default behavior, because it will not work as expected: manually enabled/disables sensors will stay disabled/enabled.

--- custom_components/thermal_comfort/__init__.py
+++ custom_components/thermal_comfort/__init__.py
@@ -4,3 +4,55 @@ Custom integration to integrate thermal_comfort with Home Assistant.
 For more details about this integration, please refer to
 https://github.com/dolezsa/thermal_comfort
 """
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant
+
+from .config_flow import get_value
+from .const import (
+    CONF_HUMIDITY_SENSOR,
+    CONF_POLL,
+    CONF_TEMPERATURE_SENSOR,
+    DOMAIN,
+    PLATFORMS,
+    UPDATE_LISTENER,
+)
+from .sensor import SensorType
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up entry configured from user interface."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        CONF_NAME: get_value(entry, CONF_NAME),
+        CONF_TEMPERATURE_SENSOR: get_value(entry, CONF_TEMPERATURE_SENSOR),
+        CONF_HUMIDITY_SENSOR: get_value(entry, CONF_HUMIDITY_SENSOR),
+        CONF_POLL: get_value(entry, CONF_POLL),
+    }
+    if entry.unique_id is None:
+        # We have no unique_id yet, let's use backup.
+        hass.config_entries.async_update_entry(entry, unique_id=entry.entry_id)
+
+    for st in SensorType:
+        hass.data[DOMAIN][entry.entry_id][st] = get_value(entry, st)
+
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    update_listener = entry.add_update_listener(async_update_options)
+    hass.data[DOMAIN][entry.entry_id][UPDATE_LISTENER] = update_listener
+    return True
+
+
+async def async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Update options from user interface."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Remove entry via user interface."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        update_listener = hass.data[DOMAIN][entry.entry_id][UPDATE_LISTENER]
+        update_listener()
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
---
+++ custom_components/thermal_comfort/config_flow.py
@@ -0,0 +1,176 @@
+"""Tests for config flows."""
+from __future__ import annotations
+
+import logging
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import entity_registry
+import voluptuous as vol
+
+from .const import (
+    CONF_HUMIDITY_SENSOR,
+    CONF_POLL,
+    CONF_TEMPERATURE_SENSOR,
+    DEFAULT_NAME,
+    DOMAIN,
+)
+from .sensor import DEFAULT_SENSOR_TYPES, SensorType
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def get_value(
+    config_entry: config_entries.ConfigEntry | None, param: str, default=None
+):
+    """Get current value for configuration parameter.
+
+    :param config_entry: config_entries|None: config entry from Flow
+    :param param: str: parameter name for getting value
+    :param default: default value for parameter, defaults to None
+    :returns: parameter value, or default value or None
+    """
+    if config_entry is not None:
+        return config_entry.options.get(param, config_entry.data.get(param, default))
+    else:
+        return default
+
+
+def build_schema(
+    config_entry: config_entries | None, show_advanced: bool = False, step: str = "user"
+) -> vol.Schema:
+    """Build configuration schema.
+
+    :param config_entry: config entry for getting current parameters on None
+    :param show_advanced: bool: should we show advanced options?
+    :param step: for which step we should build schema
+    :return: Configuration schema with default parameters
+    """
+    # ToDo: get list of CONF_TEMPERATURE_SENSOR and CONF_HUMIDITY_SENSOR to create dropdown list and "one of" selection
+    schema = vol.Schema(
+        {
+            vol.Required(
+                CONF_NAME, default=get_value(config_entry, CONF_NAME, DEFAULT_NAME)
+            ): str,
+            vol.Required(
+                CONF_TEMPERATURE_SENSOR,
+                default=get_value(config_entry, CONF_TEMPERATURE_SENSOR),
+            ): str,
+            vol.Required(
+                CONF_HUMIDITY_SENSOR,
+                default=get_value(config_entry, CONF_HUMIDITY_SENSOR),
+            ): str,
+        }
+    )
+    if show_advanced:
+        schema = schema.extend(
+            {
+                vol.Optional(
+                    CONF_POLL, default=get_value(config_entry, CONF_POLL, False)
+                ): bool,
+            }
+        )
+        if step == "user":
+            for st in SensorType:
+                default_enable = st in DEFAULT_SENSOR_TYPES
+                schema = schema.extend(
+                    {
+                        vol.Optional(
+                            str(st),
+                            default=get_value(config_entry, str(st), default_enable),
+                        ): bool
+                    }
+                )
+
+    return schema
+
+
+def check_input(hass: HomeAssistant, user_input: dict) -> dict:
+    """Check that we may use suggested configuration.
+
+    :param hass: hass instance
+    :param user_input: user input
+    :returns: dict with error.
+    """
+
+    # ToDo: user_input have ConfigType type, but it in codebase since 2021.12.10
+
+    result = {}
+
+    t_sensor = hass.states.get(user_input[CONF_TEMPERATURE_SENSOR])
+    p_sensor = hass.states.get(user_input[CONF_HUMIDITY_SENSOR])
+
+    if t_sensor is None:
+        result["base"] = "temperature_not_found"
+
+    if p_sensor is None:
+        result["base"] = "humidity_not_found"
+
+    # ToDo: we should not trust user and check:
+    #  - that CONF_TEMPERATURE_SENSOR is temperature sensor and have state_class measurement
+    #  - that CONF_HUMIDITY_SENSOR is humidity sensor and have state_class measurement
+    return result
+
+
+class ThermalComfortConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Configuration flow for setting up new thermal_comfort entry."""
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return ThermalComfortOptionsFlow(config_entry)
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        errors = {}
+
+        if user_input is not None:
+            if not (errors := check_input(self.hass, user_input)):
+                er = entity_registry.async_get(self.hass)
+
+                t_sensor = er.async_get(user_input[CONF_TEMPERATURE_SENSOR])
+                p_sensor = er.async_get(user_input[CONF_HUMIDITY_SENSOR])
+                _LOGGER.debug(f"Going to use t_sensor {t_sensor}")
+                _LOGGER.debug(f"Going to use p_sensor {p_sensor}")
+
+                if t_sensor is not None and p_sensor is not None:
+                    unique_id = f"{t_sensor.unique_id}-{p_sensor.unique_id}"
+                    await self.async_set_unique_id(unique_id)
+                    self._abort_if_unique_id_configured()
+
+                return self.async_create_entry(
+                    title=user_input[CONF_NAME], data=user_input
+                )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=build_schema(None, self.show_advanced_options),
+            errors=errors,
+        )
+
+
+class ThermalComfortOptionsFlow(config_entries.OptionsFlow):
+    """Handle options."""
+
+    def __init__(self, config_entry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+
+        errors = {}
+        if user_input is not None:
+            _LOGGER.debug(f"OptionsFlow: going to update configuration {user_input}")
+            if not (errors := check_input(self.hass, user_input)):
+                return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=build_schema(
+                self.config_entry, self.show_advanced_options, "init"
+            ),
+            errors=errors,
+        )
