410317c35e83a321caa180cde0314592f214efd1
Gabriel Rauter <rauter.gabriel@gmail.com>
2022-01-23 15:13:15 +0100

    Refactor thermal_comfort to support async_setup
    This adds async_setup to the thermal_comfort integration and allows to
    setup the platforms under integration domain key.
    e.g.
    ```yaml
    thermal_comfort:
      - sensor:
        - temperature_sensor: sensor.kitchen_temperature
          humidity_sensor: sensor.kitchen_humidity
    ```
    
    With this commit the thermal_comfort integration supports both the old
    legacy configuration under the platform key e.g. sensor and the new
    configuration under integration domain key.
    
    The `friendly_name` config key has been renamed to `name` and takes up
    the role as both friendly name and identifier = entity_id.
    This requires user intervention when converting the old
    configurations to new ones.
    
    e.g.
    ```yaml
    sensor:
      - platform: thermal_comfort
        sensors:
          old_kitchen:
            friendly_name: Old Kitchen
            temperature_sensor: sensor.kitchen_temperature
            humidity_sensor: sensor.kitchen_humidity
    ```
    would need to be changed to
    ```yaml
    thermal_comfort:
      - sensor:
        - name: Old Kitchen
          temperature_sensor: sensor.kitchen_temperature
          humidity_sensor: sensor.kitchen_humidity
    ```

---
+++ custom_components/thermal_comfort/config.py
@@ -0,0 +1,46 @@
+"""Thermal Comfort config validator."""
+
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
+from homeassistant.config import async_log_exception, config_without_domain
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.typing import ConfigType
+import voluptuous as vol
+
+from . import sensor as sensor_platform
+from .const import DOMAIN
+
+PACKAGE_MERGE_HINT = "list"
+
+CONFIG_SECTION_SCHEMA = vol.Schema(
+    {
+        vol.Optional(SENSOR_DOMAIN): vol.All(
+            cv.ensure_list, [sensor_platform.SENSOR_SCHEMA]
+        ),
+    }
+)
+
+
+async def async_validate_config(hass: HomeAssistant, config: ConfigType) -> ConfigType:
+    """Validate config."""
+    if DOMAIN not in config:
+        return config
+
+    config_sections = []
+
+    for cfg in cv.ensure_list(config[DOMAIN]):
+        try:
+            cfg = CONFIG_SECTION_SCHEMA(cfg)
+
+        except vol.Invalid as err:
+            async_log_exception(err, DOMAIN, cfg, hass)
+            continue
+
+        config_sections.append(cfg)
+
+    # Create a copy of the configuration with all config for current
+    # component removed and add validated config back in.
+    config = config_without_domain(config, DOMAIN)
+    config[DOMAIN] = config_sections
+
+    return config
