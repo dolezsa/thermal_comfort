bf5e04840665168e986174b7501f9afe566329e4
Gabriel Rauter <rauter.gabriel@gmail.com>
2022-01-24 14:16:44 +0100

    Add additional device_infos
    Add manufacturer, model and sw_version to device_info. This shows as
    Virtual Device by Thermal Comfort Fimware: 1.3.1

--- custom_components/thermal_comfort/const.py
+++ custom_components/thermal_comfort/const.py
@@ -1,11 +1,12 @@
 """General thermal_comfort constants."""
 from homeassistant.const import Platform
 
+VERSION = "1.3.1"
 DOMAIN = "thermal_comfort"
 PLATFORMS = [Platform.SENSOR]
 CONF_TEMPERATURE_SENSOR = "temperature_sensor"
 CONF_HUMIDITY_SENSOR = "humidity_sensor"
 CONF_POLL = "poll"
 
-DEFAULT_NAME = "Thermal comfort"
+DEFAULT_NAME = "Thermal Comfort"
 UPDATE_LISTENER = "update_listener"
